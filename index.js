d8570b90fc0db76d4fa9c72f0245672650c889e7
Himanshu <himanshuarya632@gmail.com>
2024-08-24 12:01:09 +0530

    bug fix

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true, limit: '16kb' }))
 app.use(cors({
-  origin: ['http://localhost:4000', 'http://localhost:5173'],
+  origin: ['https://tausif-e-commerce.onrender.com', 'https://mukul-e-commerce.onrender.com'],
   credentials: true,
 }));
 app.use(cookieParser())
