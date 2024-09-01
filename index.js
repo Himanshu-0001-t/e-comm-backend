516a3eeb7d801e675ce363c766a06b69cbc6432d
Himanshu <himanshuarya632@gmail.com>
2024-09-01 17:35:29 +0530

    bug fix

--- index.js
+++ index.js
@@ -28,12 +28,6 @@ app.use(cors({
 }));
 
 
-app.use("/api/user", userRouter)
-app.use("/api/product", productRouter)
-app.use("/api/category", categoryRouter)
-app.use("/api/cart", cartRouter)
-app.use("/api/order", orderRouter)
-
 app.get("/", (req, res) => {
   res.send("Welcome to my web page")
 })
