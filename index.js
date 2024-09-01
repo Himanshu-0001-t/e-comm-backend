8ddeda3cf01a3217737e12c763f685b6193b386a
Himanshu <himanshuarya632@gmail.com>
2024-09-01 17:30:18 +0530

    bug fix

--- index.js
+++ index.js
@@ -39,38 +39,38 @@ app.get("/", (req, res) => {
 })
 
 // Auth routes 
-app.post("api/signUp", signUp)
-app.post("api/login", login)
-app.post("api/logout", verifyUser, logout)
+app.post("/api/signUp", signUp)
+app.post("/api/login", login)
+app.post("/api/logout", verifyUser, logout)
 
 // Cart routes 
-app.post('api/cart', verifyUser, createOrUpdateCart)
-app.get('api/cart/:id', verifyUser, getCartByUserId)
-app.patch('api/cart/update', verifyUser, updateProductQuantity)
-app.post('api/cart/remove', verifyUser, removeProductFromCart)
-app.post('api/cart/clear', verifyUser, clearCart)
+app.post('/api/cart', verifyUser, createOrUpdateCart)
+app.get('/api/cart/:id', verifyUser, getCartByUserId)
+app.patch('/api/cart/update', verifyUser, updateProductQuantity)
+app.post('/api/cart/remove', verifyUser, removeProductFromCart)
+app.post('/api/cart/clear', verifyUser, clearCart)
 
 // Category routes 
-app.post('api/category', isAdmin, addCategory)
-app.get('api/categorys', showAllCategory)
-app.get('api/category/:id', getSingleCategory)
-app.patch('api/category/u', isAdmin, updateCategory)
-app.delete('api/category/d', isAdmin, deleteCategory)
+app.post('/api/category', isAdmin, addCategory)
+app.get('/api/categorys', showAllCategory)
+app.get('/api/category/:id', getSingleCategory)
+app.patch('/api/category/u', isAdmin, updateCategory)
+app.delete('/api/category/d', isAdmin, deleteCategory)
 
 // Orders routes 
-app.post('api/order', verifyUser, createOrder)
-app.get('api/orders/:id', verifyUser, getOrders)
-app.get('api/order/:id', verifyUser, getOrderById)
-app.get('api/order/u/:id', verifyUser, updateOrder)
-app.get('api/order/d/:id', verifyUser, deleteOrder)
+app.post('/api/order', verifyUser, createOrder)
+app.get('/api/orders/:id', verifyUser, getOrders)
+app.get('/api/order/:id', verifyUser, getOrderById)
+app.get('/api/order/u/:id', verifyUser, updateOrder)
+app.get('/api/order/d/:id', verifyUser, deleteOrder)
 
 // Product routes 
-app.post("api/product", isAdmin, upload.single("image"), addProduct)
-app.get("api/products", getAllProduct)
-app.get("api/product/q", filterProduct)
-app.get("api/product/:id", getSingleProduct)
-app.patch("api/product/u/:id", isAdmin, updateProduct)
-app.delete("api/product/d/:id", isAdmin, deleteProduct)
+app.post("/api/product", isAdmin, upload.single("image"), addProduct)
+app.get("/api/products", getAllProduct)
+app.get("/api/product/q", filterProduct)
+app.get("/api/product/:id", getSingleProduct)
+app.patch("/api/product/u/:id", isAdmin, updateProduct)
+app.delete("/api/product/d/:id", isAdmin, deleteProduct)
 
 
 app.listen(process.env.PORT, () => {
